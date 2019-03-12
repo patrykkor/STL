5232d9b9210bcba4413b235aeaf517a3568e8933
patrykkor <patkor9@gmail.com>
2019-03-12 12:27:32 +0100

    Dodalem konsolaplikejszyn.cpp

---
+++ konsolaplikejszyn1.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int number;
+	char s;
+	queue<int> n;
+	while (cin >> s) 
+	{
+		if(s == '+')
+		{
+			cin >> number;
+			n.push(number);
+		}
+		else if (s == '-')
+		{
+			if(!n.empty())
+			{
+				n.pop();
+			}
+			else
+			{
+				cout << "Error" << endl;
+			}
+		}
+		else
+		{
+			cout << "Que?" << endl;
+		}
+	}
+	while (!n.empty())
+	{
+		cout << n.front() << " ";
+		n.pop();
+	}
+	system("pause");
+	return 0;
+}
